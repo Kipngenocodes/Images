2f9600316a400f6795244d0f27609d4a9c457983
Kipngenocodes <kipngenoalex.c@gmail.com>
2023-10-10 10:47:32 -0600

    working Messagebox Buttons

--- Messagebuttons.py
+++ Messagebuttons.py
@@ -12,7 +12,13 @@ root.title("Working with MessageBox")
 # messagebox.showinfo()
 # messagebox.askokcancel()
 def popup():
-    messagebox.askquestion("Are you Okay", "Be okay!")
+    response= messagebox.askyesno("Question", "Are clicking unsafe Links!")
+    # mylabel=Label(root, text=response).pack()
+    if response==1:
+        Label(root, text="You Clicked Yes!").pack()
+    else:
+        Label(root, text="You Clicked a No!").pack()
+
 
 
 Button(root, text="Popup", command=popup).pack()
