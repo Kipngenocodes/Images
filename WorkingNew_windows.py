c51878f5e3bef358e430ce0d32dba0577b5602aa
Kipngenocodes <kipngenoalex.c@gmail.com>
2023-10-10 12:44:00 -0600

    Working with new windows

--- WorkingNew_windows.py
+++ WorkingNew_windows.py
@@ -2,12 +2,12 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
 
-
 # Creating an open button which have to be clicked to open second window.
 
 
 root = Tk()
 root.title("Working With New windows.")
+
 
 def open():
     global my_image
@@ -16,7 +16,9 @@ def open():
     label = Label(top, text="Helloworld").pack()
     my_image = ImageTk.PhotoImage(Image.open("hopsin5.png"))
     my_label = Label(top, image=my_image).pack()
+    buttonexit = Button(top, text="Click to close", command=top.destroy).pack()
 
-buttonclicked= Button(root, text="Click to Open", command=open).pack()
+
+buttonclicked = Button(root, text="Click to Open", command=open).pack()
 
 root.mainloop()
