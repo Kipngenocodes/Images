2d9d8a5c0105f0506b2e145e89d16e8ddbb2d6e9
Kipngenocodes <kipngenoalex.c@gmail.com>
2023-10-10 09:38:17 -0600

    Working with RadioButtons

--- Radiobuttons.py
+++ Radiobuttons.py
@@ -3,7 +3,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 root = Tk()
-
+root.title("Working With Radio Buttons")
 r = IntVar()
 
 
@@ -12,15 +12,27 @@ def Clicked(value):
     myLabel = Label(root, text=value)
     myLabel.pack()
 
+Capital_cities =[
+    ("Kenya","Nairobi"),
+    ("Uganda", "Kampala"),
+    ("Tanzania","Dar Es Salam"),
+    ("Rwanda", "Kigali"),
+    ("Burundi", "Bujumbura"),
+]
 
-root.title("Working With Radio Buttons")
-Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: Clicked(r.get())).pack()
-Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: Clicked(r.get())).pack()
+capital =StringVar()
+capital.set("Nairobi")
 
-myLabel = Label(root, text=r.get())
+for country, city in Capital_cities:
+    Radiobutton(root, text=country, variable=capital, value=city).pack()
+
+# Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: Clicked(r.get())).pack()
+# Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: Clicked(r.get())).pack()
+
+myLabel = Label(root, text=capital.get())
 myLabel.pack()
 
-mybutton = Button(root, text="Clicked here", command=lambda: Clicked(r.get()))
+mybutton = Button(root, text="Clicked here", command=lambda: Clicked(capital.get()))
 mybutton.pack()
 
 root.mainloop()
